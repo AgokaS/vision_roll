94016e7dfb185ff1b97cb1e92f7dc7877ff862ab
AgokaS <55710913+AgokaS@users.noreply.github.com>
2019-11-21 21:53:13 +0300

    Add files via upload

---
+++ vision_roll.py
@@ -0,0 +1,9 @@
+"""Основной файли проекта по компьютерному зрению
+Avtors: Denis Pankratov, Egor Sidorov """
+import vc2
+import numpy as np
+import neurolab as nl
+
+def __main__():
+    """Основная функция прогрвммы"""
+    pass
