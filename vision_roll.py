f7bddd88c5ce0aef87dfd688da221dc0db2571f5
Егор Сидоров <egor25sidorov@gmail.com>
2019-11-21 21:58:19 +0300

    test

--- vision_roll.py
+++ vision_roll.py
@@ -1,9 +1,10 @@
 """Основной файли проекта по компьютерному зрению
 Avtors: Denis Pankratov, Egor Sidorov """
-import vc2
+import cv2
 import numpy as np
 import neurolab as nl
 
 def __main__():
     """Основная функция прогрвммы"""
+    cv2.imread()
     pass
